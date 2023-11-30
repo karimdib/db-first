078ee761429c310b918ec6b086745982c9cd1ed4
karimdib <karim.dib2001@gmail.com>
2023-11-30 20:47:01 +0100

    create table with php

--- carz/index.php
+++ carz/index.php
@@ -3,7 +3,7 @@
 
 //$connessione = mysqli_connect('localhost', 'root', '', 'primo');
 
-$connessione = new mysqli('localhost', 'root', '');
+$connessione = new mysqli('localhost', 'root', '', 'primo');
 
 if ($connessione === false) {
     echo 'errore con la connessione';
@@ -11,10 +11,27 @@ if ($connessione === false) {
     echo 'connsessione avvenuta con successco';
 }
 
+/*
 $database = ("CREATE DATABASE db_prova");
 
 if ($connessione->query($database) === true) {
     echo 'database creato con successo';
 } else {
     echo 'errore con il database';
+}
+*/
+
+
+
+$table = "CREATE TABLE persone(
+    id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(40) NOT NULL,
+    cognome VARCHAR(40) NOT NULL,
+    email VARCHAR(90) NOT NULL UNIQUE
+)";
+
+if ($connessione->query($table) === true) {
+    echo 'la tabella è stata creata con successo  ';
+} else {
+    echo 'cè stato un errore con la creazione della tabella';
 }
