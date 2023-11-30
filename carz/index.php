80b6664f35aab895ae81449fab3ed79e9f60cba9
karimdib <karim.dib2001@gmail.com>
2023-11-30 20:32:03 +0100

    create database with php query

--- carz/index.php
+++ carz/index.php
@@ -3,10 +3,18 @@
 
 //$connessione = mysqli_connect('localhost', 'root', '', 'primo');
 
-$connessione = new mysqli('localhost', 'root', '', 'primo');
+$connessione = new mysqli('localhost', 'root', '');
 
 if ($connessione === false) {
     echo 'errore con la connessione';
 } else {
     echo 'connsessione avvenuta con successco';
+}
+
+$database = ("CREATE DATABASE db_prova");
+
+if ($connessione->query($database) === true) {
+    echo 'database creato con successo';
+} else {
+    echo 'errore con il database';
 }
